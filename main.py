47d2bb8379cdecc181ff55e14b2b3032fe14a292
3XTR4OS <84981999+3XTR4OS@users.noreply.github.com>
2021-07-27 17:38:30 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,51 @@
-# Версионирование метаданных SAS
-import os
+# Задача: Версионирование метаданных SAS
 import shutil
+import os
 
-FILE_FOR_EXPORT = 'C://Users//team53//PycharmProjects//I_am_POWER//For_export'
+PATH_FOR_EXPORT = 'C://Users//team53//PycharmProjects//I_am_POWER//For_export'
 
 
 class Versioning:
 
     def __init__(self, data):
-        """В data записывается path файла"""
+        """В data записывается path исходного файла"""
         self.data = data  # Предположим, что это метаданные sas
         self.block_mark = None
 
-    def data_block(self, mark_to_block):  # mark_to_block - путь, который запретит перемещать файл
+    def data_block(self, mark_to_block):
+        """метка, которая запретит экспортировать файл каталога"""
         self.block_mark = mark_to_block
         return self.block_mark
 
-    def export(self, mark='all'):  # Экспорт файлов куда-либо по метке
+    def export(self, mark='all'):
 
         block_mark = self.block_mark
 
         try:
-            if self.block_mark == self.data:  # Проверка на доступ
+            if self.block_mark == self.data:  # Проверка файла на доступ
                 return 'Данный файл(ы) запрещены для перемещения.'
 
             elif mark == 'all':
-                shutil.move(self.data, FILE_FOR_EXPORT)
-                return f'Успешное перемещение в {FILE_FOR_EXPORT}'
+                shutil.move(self.data, PATH_FOR_EXPORT)
+                return f'Успешное перемещение в {PATH_FOR_EXPORT}'
 
-            """Цикл, запрещающий отдельный файл"""
+            """Цикл, проверяющий отдельный файл каталога на запрет к перемещению."""
             for file in os.listdir(self.data):
                 if file == block_mark and mark == file:
                     return 'Данный файл(ы) запрещены для перемещения.'
 
             else:
-                shutil.move(self.data + '//' + mark, FILE_FOR_EXPORT)
+                shutil.move(self.data + '//' + mark, PATH_FOR_EXPORT)
                 return 'Успешное перемещение', self.data + '//' + mark
 
         except FileNotFoundError:
             print(None)
 
-    def git(self):  # идей нет
+    def git(self):
         pass
 
     def get_data_content(self):
-        """Для себя"""
+        """Функция, возвращающая содержимое txt файла"""
 
         try:
             with open(self.data, 'r') as file:
@@ -54,6 +55,7 @@ class Versioning:
         except PermissionError:
 
             files = os.listdir(self.data)
+            error_message = 'Файл не найден.'
             return files
 
 
@@ -67,11 +69,11 @@ def demonstration_data_block():
 
 def demonstration_export():
     """В данной демонстрации для взаимодействий запрещён только один отдельный файл каталога
-    + Экспорт файла в папочку For_export"""
+    + Экспорт файла в папку For_export"""
     data2 = Versioning('C://Users//team53//PycharmProjects//I_am_POWER//my_files')
     data2.data_block('cars12.txt')
     print(data2.export('cars3.txt'))
     print(data2.export('cars12.txt'))
-
+#
 # demonstration_data_block()
 # demonstration_export()
